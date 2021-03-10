9fa8af51440343b29327f8f63bef466ab8be8e55
is721101 <79496989+is721101@users.noreply.github.com>
2021-03-09 21:51:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ app.get("/",(req,res)=>{
     res.send("Prueba Heroku Carlos Iván Undiano Herrera")
   })
 
-  var port_number = server.listen(process.env.PORT || 3000);
-  app.listen(port_number);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
   module.exports = app;
